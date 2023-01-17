c4abac34ef43ba877fd3cc1ec3cc05831faa5c7d
Safoh Sassa <32834244+safoh-sassa@users.noreply.github.com>
2023-01-17 10:09:33 +0100

    Create data.js
    data

---
+++ src/data.js
@@ -0,0 +1,22 @@
+ export const parentsData = [
+  {
+    id: "ID_01",
+    firstName: "Karel",
+    lastName: "Novák"
+  },
+  {
+    id: "ID_02",
+    firstName: "Jakub",
+    lastName: "Svoboda"
+  },
+  {
+    id: "ID_03",
+    firstName: "Anna",
+    lastName: "Veselá"
+  },
+  {
+    id: "ID_04",
+    firstName: "Karel",
+    lastName: "Nový"
+  }
+]
